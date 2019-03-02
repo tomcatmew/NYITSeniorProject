05af731f77f065d7e622a53d0a2d48664f873a8e
TomCat <helltomcat@gmail.com>
2019-03-02 06:01:18 -0500

    Add files via upload

---
+++ FrontEndTest/class.js
@@ -0,0 +1,125 @@
+function c501()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c502()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> CSCI-330 ");
+}
+
+function c503()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> BIOI-101 ");
+}
+
+function c504()
+{
+  $( "#textbox" ).html("Room is under construction");
+}
+
+function c505()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c506()
+{
+  $( "#textbox" ).html("In Class : <br/> CSCI-318 <br/> Professor :  <br/> Taylor");
+}
+
+// 6 floor
+
+function c601()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c602()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c603()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> CHEM-302 ");
+}
+
+function c604()
+{
+  $( "#textbox" ).html("Room is under construction");
+}
+
+function c605()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c606()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+// 7 floor
+
+function c701()
+{
+  $( "#textbox" ).html("In Class : <br/> CSCI-201 <br/> Professor :  <br/> Chris");
+}
+
+function c702()
+{
+  $( "#textbox" ).html("In Class : <br/> INIT-203 <br/> Professor :  <br/> Lee");
+}
+
+function c703()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> IENG-400 ");
+}
+
+function c704()
+{
+  $( "#textbox" ).html("Room is under construction");
+}
+
+function c705()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c706()
+{
+  $( "#textbox" ).html("In Class : <br/> BIOI-220 <br/> Professor :  <br/> King");
+}
+
+// 8 floor
+
+function c801()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c802()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> CSCI-102 ");
+}
+
+function c803()
+{
+  $( "#textbox" ).html("Class Cancelled : <br/> CSCI-400 ");
+}
+
+function c804()
+{
+  $( "#textbox" ).html("Room is under construction");
+}
+
+function c805()
+{
+  $( "#textbox" ).html("Room is Free");
+}
+
+function c806()
+{
+  $( "#textbox" ).html("In Class : <br/> TECH <br/> Professor :  <br/> Wang");
+}
