b20874819d0912b05e58ca987955366012293015
~FuzzDragon <35173613+ZDragon1996@users.noreply.github.com>
2019-03-21 13:34:56 -0400

    Add files via upload

--- SeniorProject/www/scriptTest.js
+++ SeniorProject/www/scriptTest.js
@@ -43,13 +43,24 @@ function courseDescription(room,location,time){
   for(var i = 0; i < tempTimesString.length; i++){
     times.push(tempTimesString[i]);
   }
-  if (times.length == 3){
+  if (times.length == 3 && rooms.length == 3){
+      //meet 3 times with 3 room
     result = `${locations[0]} ${rooms[0]} ${times[0]} ${locations[1]} ${rooms[1]} ${times[1]} ${locations[2]} ${rooms[2]} ${times[2]}`;
   }
-  else if (times.length == 2){
+  else if (times.length == 2 && rooms.length == 2){
+      //meet 2 times with 2 room
     result = `${locations[0]} ${rooms[0]} ${times[0]} ${locations[1]} ${rooms[1]} ${times[1]}`;
-  }else{
+  }
+  else if (times.length == 2 && rooms.length == 1){
+    //meet 2 times with 1 room
+    result = `${locations[0]} ${rooms[0]} ${times[0]} ${locations[1]} ${rooms[0]} ${times[1]}`;
+  }
+  else if (times.length == 1 && rooms.length == 1){
+    //meet 1 times with 1 room
     result = `${locations[0]} ${rooms[0]} ${times[0]}`;
+  }
+  else{
+    console.log("Condition is not found");
   }
 
   return result;
